86cafb6a23d621238d67409cef2549ba5b12836f
Marwan Mekhamer <Marwan-Mekhamer12@gmail.com>
2026-04-09 16:20:10 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Marwan Mekhamer on 09/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
